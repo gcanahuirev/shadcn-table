ef3eeee6564c732adfddc77c2078557034939553
Giuseppe <giuseppecv56@outlook.com>
2023-11-03 23:46:17 -0500

    fix: hydration failed because the initial ui does not match

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,9 +1,21 @@
 "use client";
 
-import * as React from "react";
+import { useEffect, useState } from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  const [isClient, setIsClient] = useState(false);
+
+  // This is a side effect that runs after the first render and sets the isClient state to true
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  // This is a conditional rendering that returns null if the component is not client
+  if (!isClient) {
+    return null;
+  }
+
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
