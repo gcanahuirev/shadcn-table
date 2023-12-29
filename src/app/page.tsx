c2e3a56eac88116899c80f2777ed6240161930ac
Giuseppe <giuseppecv56@outlook.com>
2023-12-28 20:35:38 -0500

    build: update deps and devDeps

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <Button>Hello</Button>
-  )
+    <Button asChild>
+      <Link href="/people">Hello</Link>
+    </Button>
+  );
 }
